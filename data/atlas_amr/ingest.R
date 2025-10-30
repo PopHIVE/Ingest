6ded154de7c207caacd363e1b698b49a89107062
weinbergerlab <dweinber@gmail.com>
2025-10-30 14:24:05 -0400

    format the parquet

--- data/atlas_amr/ingest.R
+++ data/atlas_amr/ingest.R
@@ -1,12 +1,21 @@
-#
-# Download
-#
+library(tidyverse)
+library(arrow)
 
-# add files to the `raw` directory
+# a1 <- readxl::read_excel('./raw/staging/2025_03_11 atlas_antibiotics.xlsx', guess_max = 5000)
+# 
+#  arrow::write_parquet(a1,'./raw/staging/2025_03_11 atlas_antibiotics.parquet' )
 
-#
-# Reformat
-#
+a1 <- arrow::open_dataset('./raw/staging/2025_03_11 atlas_antibiotics.parquet') %>%
+  filter(Country=='United States') %>%
+  dplyr::select(Species,  State, `Age Group`, Source, Year) %>%
+  collect()
 
-# read from the `raw` directory, and write to the `standard` directory
 
+a2 <- a1 %>% 
+  group_by(Species) %>%
+  mutate(N_total_species = n()) %>%
+  filter(N_total_species >=1000) %>%
+  ungroup() %>%
+  group_by(Species,  Year) %>%
+  summarize(N=n()) %>%
+  mutate(N = if_else(N<10,NA_real_,N))
