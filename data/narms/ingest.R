6da554d2ada3e783629c618d584c43ad0bdc27e3
tobysalmon <toby.salmon@yale.edu>
2026-01-15 15:03:16 -0500

    updated ingest.R - filing in standard

--- data/narms/ingest.R
+++ data/narms/ingest.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(dcf)
+library(cdlTools)
 
 #
 # Download and add files to the raw directory
@@ -12,11 +13,49 @@ raw_state <- dcf::dcf_download_cdc(
   process$raw_state
 )
 
+#
+# Reformat (only if raw data has changed)
+#
+
 if (!identical(process$raw_state, raw_state)) {
-  data_raw <- vroom::vroom("./raw/jbhn-e8xn.csv.xz", show_col_types = FALSE) 
-    
-    
-    # record processed raw state
-    process$raw_state <- raw_state
-    dcf::dcf_process_record(updated = process)
+  
+  data_raw <- vroom::vroom("./raw/jbhn-e8xn.csv.xz", show_col_types = FALSE)
+  
+  data_standard <- data_raw %>%
+    mutate(
+      # Convert state abbreviations to FIPS codes
+      geography = sprintf("%02d", fips(State, to = "FIPS")),
+      # Create proper date (end of month)
+      time = lubridate::ceiling_date(
+        lubridate::make_date(Year, Month, 1), 
+        "month"
+      ) - 1
+    ) %>%
+    rename(
+      pathogen = Pathogen,
+      serotype = `Serotype/Species`,
+      source_type = `Source Type`,
+      source_site = `Source Site`,
+      narms_isolates = `Number of isolates`,
+      narms_outbreak_isolates = `Outbreak associated isolates`,
+      narms_new_outbreaks = `New multistate outbreaks`,
+      narms_new_outbreaks_us = `New multistate outbreaks - US`,
+      narms_pct_amr = `% Isolates with clinically important antimicrobial resistance`,
+      narms_sequenced = `Number of sequenced isolates analyzed by NARMS`
+    ) %>%
+    select(
+      time, geography, pathogen, serotype, source_type, source_site,
+      starts_with("narms_")
+    )
+  
+  # Write standard data
+  vroom::vroom_write(
+    data_standard,
+    "standard/data.csv.gz",
+    ","
+  )
+  
+  # Record processed raw state
+  process$raw_state <- raw_state
+  dcf::dcf_process_record(updated = process)
 }
